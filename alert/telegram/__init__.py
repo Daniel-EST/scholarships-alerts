712a73a672dad76f3e7d6c4bb9174a66e1f59b45
Daniel-EST <dd_santos@id.uff.br>
2022-02-12 19:56:33 -0300

    Fix telegram images

--- alert/telegram/__init__.py
+++ alert/telegram/__init__.py
@@ -8,7 +8,7 @@ CHAT_ID = int(os.environ.get("TELEGRAM_CHAT_ID"))
 
 def send_message(data):
     bot = Bot(TOKEN)
-    with open(f"alert/telegram/img/{data['country']}.png", 'rb') as country_flag:
+    with open(f"./alert/telegram/img/{data['country'].lower()}.png", 'rb') as country_flag:
         caption = f"Olá, Aparentemente as inscrições do <b>{data['name']}</b> para <b>{data['program']}</b> já estão abertas!\nEntre no site do <a href='{data['url']}'>{data['name']}</a> e saiba mais."
         bot.send_photo(
             chat_id=CHAT_ID, 
